d0f1460bc2a1ecd4cf5f02c83ba05e95fa7f984e
Sinon <caothanhhien62@gmail.com>
2023-03-27 06:48:54 -0700

    1.3.2 - idk

--- src/main/java/live/karyl/anifetch/providers/AnimeProvider.java
+++ src/main/java/live/karyl/anifetch/providers/AnimeProvider.java
@@ -10,34 +10,38 @@ import live.karyl.anifetch.models.AnimeSource;
 import live.karyl.anifetch.utils.Utils;
 
 import java.util.List;
+import java.util.Locale;
 
 public abstract class AnimeProvider {
-    protected final String siteName;
-    protected final String baseUrl;
+	protected final String siteName;
+	protected final String baseUrl;
 
-    protected final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36";
-    protected final String PROXY_VN = "https://proxy-vn.karyl.live/v1/server/proxy?link=";
+	protected final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36";
+	protected final String PROXY_VN = "https://proxy-vn.karyl.live/v1/server/proxy?link=";
 
-    protected final OkHttp connection = AniFetchApplication.getConnection();
-    protected final Redis redis = AniFetchApplication.getRedis();
-    protected final PostgreSQL postgreSQL = AniFetchApplication.getPostgreSQL();
+	protected final OkHttp connection = AniFetchApplication.getConnection();
+	protected final Redis redis = AniFetchApplication.getRedis();
+	protected final PostgreSQL postgreSQL = AniFetchApplication.getPostgreSQL();
 
-    public AnimeProvider(String siteName, String baseUrl) {
-        this.siteName = siteName;
-        this.baseUrl = baseUrl;
-    }
+	public AnimeProvider(String siteName, String baseUrl) {
+		this.siteName = siteName;
+		this.baseUrl = baseUrl;
+	}
 
-    public abstract AnimeParser search(AnilistInfo anilistInfo);
+	public abstract AnimeParser search(AnilistInfo anilistInfo);
 
-    public abstract AnimeSource getLink(String value);
+	public abstract AnimeSource getLink(String value);
 
-    protected static int extractNumberFromString(String s) {
+	protected static int extractNumberFromString(String s) {
+        s = s.toLowerCase(Locale.ROOT);
         if (s.contains("-")) {
             return Integer.parseInt(s.split("-")[0]);
-        } else if (s.endsWith("_End")) {
-            return Integer.parseInt(s.replace("_End", ""));
+        } else if (s.endsWith("_end")) {
+            return Integer.parseInt(s.replace("_end", ""));
         } else if (s.contains(".")) {
             return Integer.parseInt(Math.round(Double.parseDouble(s)) + "");
+        } else if (s.contains("full")) {
+            return 1;
         } else {
             return Integer.parseInt(s);
         }
